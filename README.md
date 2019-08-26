f9c06efa70e582b7a7cd35a93858790aaf9f6811
EgorBacon <egorpunanov@gmail.com>
2019-08-26 21:06:24 +1000

    InitialCommit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Jarvis
 This is the Repo for my OpenCV facial recognition iron-man themed project
+
+I am very much a beginner when it comes to python, and most of my code was just research so this isn't a polished project.
+
+TODO:
+1. Fix basic error
+2. Make a deep learned library
+3. Add face learning on the go functionality
+4. Add Raspi Zero
+
+Usage
+My code also looks at the folder names of images so:
+1. In folder images make a folder for each person you have photos of (e.g will-smith)
+2. run faces-train.py
+3. run face-detect.py (Doesn't work has error and face recognition is very bad)
+
+This is a just an ongoing project for me so...this doesn't work. However if you have any tips, please explain more because I am keen to learn more about this.
+
+Will post on this repo when it will.
---
+++ face-detect.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+import pickle
+
+face_cascade = cv2.CascadeClassifier('C:\\Users\\Egor\\Desktop\\Work\\Jarvis\\data\\haarcascade_frontalface_alt2.xml')
+recognniser = cv2.face.LBPHFaceRecognizer_create()
+labels = {"person's name": 1}
+recognniser.read("trainer.yml")
+with open("labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+	#capture frame by frame
+	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+
+	for (x,y,w,h) in faces:
+		#finding where the face is
+		#print(x,y,w,h)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = frame[y:y+h, x:x+w]
+
+		#recognize? Deep learning model to predict things
+		id_, conf = recognniser.predict(roi_gray)
+		if conf>=50: #and conf<=60:
+			print(id_)
+			print(labels[id_])
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			name = labels[id_]
+			color(255)
+			stroke = 2
+			cv2.putText(frame, name (x,y), font, 1, color, stroke, cv2.LINE_AA)
+		else:
+			print("No, common faces detected")
+
+		img_item = "my-image.png"
+		cv2.imwrite(img_item, roi_gray)
+
+		#making the square around the face
+		color = (0, 255, 0) #BRG
+		stroke = 2
+		end_cord_x = x+w
+		end_cord_y = y+h
+		cv2.rectangle(frame, (x,y), (end_cord_x,end_cord_y), color, stroke)
+
+	#display result frame
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(20) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows
